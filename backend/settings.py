3abdbd4b5223f95bb76fbe6518ee0da35cf89a09
Anujverma89 <panuj7295@gmail.com>
2025-05-30 20:47:37 +0530

    db removed

--- backend/settings.py
+++ backend/settings.py
@@ -77,17 +77,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ubntai',
-        'USER': 'ubntai',
-        'PASSWORD': 'ubntaihome',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    },
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
